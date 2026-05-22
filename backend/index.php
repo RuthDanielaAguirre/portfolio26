d0d85e12c2e44f1ab317ab83940d3735c9905b34
RuthDanielaAguirre <148866258+RuthDanielaAguirre@users.noreply.github.com>
2026-05-22 13:57:39 +0200

    test webhook2

--- backend/index.php
+++ backend/index.php
@@ -157,4 +157,6 @@ $payload = [
 
 echo json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 echo "<!-- pipeline -->";
+echo "<!-- pipeline -->";
+echo "<!-- pipeline -->";
 ?>
